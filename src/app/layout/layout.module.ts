f3f4769486bbcf714d14e69b11c39a6c7a89f154
rodrigodesp <rodrigodesp@gmail.com>
2020-07-07 23:25:50 -0300

    2 files modified

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -2,16 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { LayoutComponent } from './layout.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
   declarations: [
-    LayoutComponent
+    LayoutComponent,
+    HeaderComponent
   ],
   exports : [
-    LayoutComponent    
+    LayoutComponent        
   ]
 })
 export class LayoutModule { }
